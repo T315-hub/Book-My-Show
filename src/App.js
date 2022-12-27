87a14535372c7068e90efd5f376b40e6d1e8229f
ANKIT <85053976+T315-hub@users.noreply.github.com>
2022-12-27 12:26:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+function App() {
+  return (
+    <h1 className="text-3xl font-light underline">
+    Hello world!
+  </h1>
+  );
+}
+
+export default App;
